1cf515a43baf4aca88a7cc3385d2155c8f2a6b56
deve1 <nayiturikithierry401@gamil.com>
2025-10-08 13:52:43 +0200

    qwertyu

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['localhost'],
   },
-}
+  distDir: '.next', // default, safe
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
