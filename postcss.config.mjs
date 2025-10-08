252e3b83efa68faa0f01c1a7299850af2c78aa76
deve1 <nayiturikithierry401@gamil.com>
2025-10-08 13:40:05 +0200

    updated

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,7 @@
-/** @type {import('postcss').ProcessOptions} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 };
-
-export default config;
